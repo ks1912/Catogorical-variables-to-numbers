2f7947a8b8ebe9b621ef6b3262c8f9c4f7e3cc30
Kunal Singh <46413519+ks1912@users.noreply.github.com>
2019-10-21 02:42:52 +0530

    Update Datapreprocessing.py

--- Datapreprocessing.py
+++ Datapreprocessing.py
@@ -1,14 +1,14 @@
 # DATA PREPROCESSING
 # Importing the libraries 
---------------------------------------------------------------------------
+=========================================================================
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
---------------------------------------------------------------------------
+=========================================================================
 # IMPORTING DATA SET'S
 
 dataset = pd.read_csv('editeddata.csv')
---------------------------------------------------------------------------
+=========================================================================
 # Creating Veriable Vector's
 
 # Creating independent variable vector
@@ -20,14 +20,14 @@ X = dataset.iloc[:,:-1].values
 
 Y = dataset.iloc[:,3].values 
 # print(Y) Uncomment Y to print Y
---------------------------------------------------------------------------
+=========================================================================
 # Finding mean and updating it in the dataset
 
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = "NaN",strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:, 1:3])
 X[:,1:3] = imputer.transform(X[:,1:3])
---------------------------------------------------------------------------
+=========================================================================
 # Changing Catogorical data from lavels to labels and unbiasing them
 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
