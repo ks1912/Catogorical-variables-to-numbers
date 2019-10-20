36528d671eb3683d1c80bc411fc46fa33eb935db
Kunal Singh <46413519+ks1912@users.noreply.github.com>
2019-10-21 02:41:00 +0530

    Update Datapreprocessing.py

--- Datapreprocessing.py
+++ Datapreprocessing.py
@@ -1,31 +1,42 @@
-# -*- coding: utf-8 -*-
 # DATA PREPROCESSING
 # Importing the libraries 
-
+=========================================================================
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-
+=========================================================================
 # IMPORTING DATA SET'S
-datasetnew = pd.read_csv('Data.csv')
-X = datasetnew.iloc[:,:-1].values # Creating independent variable vector
-# print(X)
-Y = datasetnew.iloc[:,3].values # Creating dependent variable vector
-# print(Y)
-
+--------------------------------------------------------------------------
+dataset = pd.read_csv('editeddata.csv')
+=========================================================================
+# Creating Veriable Vector's
+--------------------------------------------------------------------------
+# Creating independent variable vector
+--------------------------------------------------------------------------
+X = dataset.iloc[:,:-1].values
+# print(X) # Uncommnet X to print X
+--------------------------------------------------------------------------
+# Creating dependent variable vector
+--------------------------------------------------------------------------
+Y = dataset.iloc[:,3].values 
+# print(Y) Uncomment Y to print Y
+==========================================================================
+# Finding mean and updating it in the dataset
+--------------------------------------------------------------------------
 from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = "nan",strategy = 'mean',axis=0)
+imputer = Imputer(missing_values = "NaN",strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:, 1:3])
 X[:,1:3] = imputer.transform(X[:,1:3])
-
+=========================================================================
+# Changing Catogorical data from lavels to labels and unbiasing them
+--------------------------------------------------------------------------
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 lableencoder_X = LabelEncoder()
 X[:, 0] = lableencoder_X.fit_transform(X[:, 0])
 onehotencoder = OneHotEncoder(categorical_features = [0])
 X = onehotencoder.fit_transform(X).toarray()
-
+--------------------------------------------------------------------------
 lableencoder_Y = LabelEncoder()
 Y = lableencoder_Y.fit_transform(Y)
-
+=========================================================================
 
